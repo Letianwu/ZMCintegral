fee1dc960699dc7b3a03ca321d927fd725a29eed
Hongzhong WU <40189288+Letianwu@users.noreply.github.com>
2018-10-23 19:21:06 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="ZMCintegral",
+    packages=['ZMCintegral'],
+    version="2.2",
+    description="Easy to use python package for Multidimensional-multi-gpu Monte Carlo integration",
+    author='ZHANG Junjie',
+    author_email='zjacob@mail.ustc.edu.cn',
+    url='https://github.com/Letianwu/ZMCintegral',
+    keywords=['Monte Carlo integraion','multi-gpu'],
+    install_requires=['tensorflow-gpu=1.10'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Apache2.0',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3.3'
+    ]
+)
