f47527a7bb21980b3dd57abd2674c802dfd96cb2
Hongzhong WU <40189288+Letianwu@users.noreply.github.com>
2019-01-19 19:51:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name="ZMCintegral",
     packages=['ZMCintegral'],
-    version="2.2",
+    version="3.0",
     description="Easy to use python package for Multidimensional-multi-gpu Monte Carlo integration",
     author='ZHANG Junjie',
     author_email='zjacob@mail.ustc.edu.cn',
     url='https://github.com/Letianwu/ZMCintegral',
     keywords=['Monte Carlo integraion','multi-gpu'],
-    install_requires=['tensorflow-gpu'],
+    install_requires=['numba'],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
@@ -21,6 +21,6 @@ setup(
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: Linux',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.6'
     ]
 )
